79c288d0979fe789f1ff851942a4c31f3924419b
Chinenye Anikwenze <anikwenzekelly@gmail.com>
2023-08-12 10:29:25 +0100

    initial commit: installing tailwind, react icons

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import { Sidebar } from "../components/Sidebar";
+
+export const Dashboard = () => {
+  return (
+    <main className="flex">
+      <Sidebar />
+      Dashboard
+    </main>
+  );
+};
+
+export default Dashboard;
