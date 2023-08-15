d16904baaa4abf2d001eb6c44dcfff77dfcbf6cf
Chinenye Anikwenze <anikwenzekelly@gmail.com>
2023-08-15 14:14:18 +0100

    refactor: fix styling on mobile screen plus minor modifications on grid display

--- src/components/NotesCard.jsx
+++ src/components/NotesCard.jsx
@@ -31,7 +31,7 @@ export const NotesCard = () => {
             onClick={handleCloseConfirmationModal}
           ></div>
           {selectedNote && (
-            <section className="absolute z-50 w-full lg:w-[80%] top-0">
+            <section className="absolute z-50 w-[90%] lg:w-[80%]">
               <ConfirmationModal />
             </section>
           )}
@@ -51,35 +51,52 @@ export const NotesCard = () => {
         });
 
         return (
-          <section className="rounded-sm shadow-lg " key={index}>
+          <section
+            className="rounded-sm shadow-lg flex flex-col justify-between"
+            key={index}
+          >
             <div className="mx-6 my-4 border-b border-gray-light">
-              <h2 className="font-medium text-base  mb-4">{title}</h2>
-              <p className="font-normal text-sm mb-4">{content}</p>
-              <p className="font-normal text-sm mb-4">{formattedDate}</p>
+              <h2 className="font-medium text-lg mb-4">{title}</h2>
+              <p className="font-normal text-sm mb-4 pb-4">
+                {content.slice(0, 200)}{" "}
+                <span
+                  onClick={() => {
+                    handleSelectNote(notes);
+                    setIsEditingNote(true);
+                    handleOpenFormModal();
+                  }}
+                  className="text-blue-600 underline cursor-pointer"
+                >
+                  {content?.length > 200 && ". Read more... "}
+                </span>
+              </p>
             </div>
-            <div className="mx-6 my-4 flex gap-3">
-              <button
-                className="bg-gray-900 rounded-full flex p-2 items-center justify-center cursor-pointer"
-                onClick={() => {
-                  handleSelectNote(notes);
-                  setIsEditingNote(true);
-                  handleOpenFormModal();
-                }}
-              >
-                <BiSolidEditAlt size={16} color="white" />
-              </button>
-              <button
-                className="bg-red-600 rounded-full flex p-2 items-center justify-center cursor-pointer"
-                onClick={() => {
-                  handleSelectNote(notes);
-                  handleOpenConfirmationModal();
-                }}
-              >
-                <AiOutlineDelete size={16} color="white" />
-              </button>
-              <span className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded ">
-                {tag}
-              </span>
+            <div className="mx-6 my-4">
+              <p className="font-normal text-xs italic mb-4">{formattedDate}</p>
+              <div className=" flex gap-3 ">
+                <button
+                  className="bg-gray-900 rounded-full flex p-2 items-center justify-center cursor-pointer"
+                  onClick={() => {
+                    handleSelectNote(notes);
+                    setIsEditingNote(true);
+                    handleOpenFormModal();
+                  }}
+                >
+                  <BiSolidEditAlt size={16} color="white" />
+                </button>
+                <button
+                  className="bg-red-600 rounded-full flex p-2 items-center justify-center cursor-pointer"
+                  onClick={() => {
+                    handleSelectNote(notes);
+                    handleOpenConfirmationModal();
+                  }}
+                >
+                  <AiOutlineDelete size={16} color="white" />
+                </button>
+                <span className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded ">
+                  {tag}
+                </span>
+              </div>
             </div>
           </section>
         );
